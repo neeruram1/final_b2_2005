895923ef42416433f1b46a60e5400c004501e83f
“Neeru <neeram85@gmail.com>
2020-08-03 09:54:38 -0600

    User story 1 complete

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -4,12 +4,15 @@ RSpec.describe 'Flight show page' do
   before(:each) do
     @delta = Airline.create(name: "Delta")
     @flight_1 = @delta.flights.create(number: 1, date: "08/03/2020", time: "5:00 PM", departure_city: "Denver", arrival_city: "Boston")
+    @flight_2 = @delta.flights.create(number: 2, date: "08/04/2020", time: "7:00 AM", departure_city: "Denver", arrival_city: "New York")
     @jim = Passenger.create(name: "Jim Halpert", age: 30)
     @pam = Passenger.create(name: "Pam Beesly", age: 28)
     @stanley = Passenger.create(name: "Stanley Hudson", age: 45)
+    @michael = Passenger.create(name: "Michael Scott", age: 43)
     PassengerFlight.create(flight: @flight_1, passenger: @jim)
     PassengerFlight.create(flight: @flight_1, passenger: @pam)
     PassengerFlight.create(flight: @flight_1, passenger: @stanley)
+    PassengerFlight.create(flight: @flight_2, passenger: @michael)
   end
 
   it "I see all of the flight information" do
@@ -28,11 +31,19 @@ RSpec.describe 'Flight show page' do
 
   it "I see the names of all the passengers on this flight" do
     visit "/flights/#{@flight_1.id}"
-    
+
     within ".passengers" do
       expect(page).to have_content(@jim.name)
       expect(page).to have_content(@pam.name)
       expect(page).to have_content(@stanley.name)
+    end
+  end
+
+  it "I do not see passengers who are not on this flight" do
+    visit "/flights/#{@flight_1.id}"
+
+    within ".passengers" do
+      expect(page).to_not have_content(@michael.name)
     end
   end
 end
