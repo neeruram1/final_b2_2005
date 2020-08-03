280a4e9af842d49cd90b6fa8c917ce1c2224a16c
“Neeru <neeram85@gmail.com>
2020-08-03 09:15:16 -0600

    Validation tests for Flight model passing

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,7 @@
 class Flight < ApplicationRecord
-end 
+  validates_presence_of :number
+  validates_presence_of :date
+  validates_presence_of :time
+  validates_presence_of :departure_city
+  validates_presence_of :arrival_city 
+end
