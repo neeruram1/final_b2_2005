eae33d4d58ab51d3ac02b52e96cee0c61a50dc81
“Neeru <neeram85@gmail.com>
2020-08-03 09:14:19 -0600

    Create Flights table

---
+++ db/migrate/20200803151347_create_flights.rb
@@ -0,0 +1,12 @@
+class CreateFlights < ActiveRecord::Migration[5.1]
+  def change
+    create_table :flights do |t|
+      t.integer :number
+      t.string :date
+      t.string :time
+      t.string :departure_city
+      t.string :arrival_city
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200803150949) do
+ActiveRecord::Schema.define(version: 20200803151347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airlines", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.integer "number"
+    t.string "date"
+    t.string "time"
+    t.string "departure_city"
+    t.string "arrival_city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
