733422f6ca1195ab58b322bbcfbda841141b3a72
“Neeru <neeram85@gmail.com>
2020-08-03 11:35:33 -0600

    One last refactor

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -4,15 +4,15 @@ class PassengerFlightsController < ApplicationController
     flight = Flight.find_by_number(params[:flight_number])
 
     if flight.nil?
-      redirect_to "/passengers/#{params[:passenger_id]}"
+      redirect_to "/passengers/#{passenger.id}"
       flash[:error] = "Flight number can't be blank"
     elsif
       passenger.flight_numbers.include?(flight.number)
-      redirect_to "/passengers/#{params[:passenger_id]}"
+      redirect_to "/passengers/#{passenger.id}"
       flash[:error] = "This flight already exists"
     else
       PassengerFlight.create(flight: flight, passenger: passenger)
-      redirect_to "/passengers/#{params[:passenger_id]}"
+      redirect_to "/passengers/#{passenger.id}"
     end
   end
 end
