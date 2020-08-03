d205ce2000cbee52e565a3f795a6d0da196449e6
“Neeru <neeram85@gmail.com>
2020-08-03 10:43:07 -0600

    Completed form to add flight from passenger show page

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -1,5 +1,7 @@
 class PassengerFlightsController < ApplicationController
   def create
-
+    flight = Flight.find_by_number(params[:flight_number])
+    PassengerFlight.create(flight_id: flight.id , passenger_id: params[:passenger_id])
+    redirect_to "/passengers/#{params[:passenger_id]}"
   end
 end
