a3e0cb00121978aff00f9ae8f64ccae72152ab29
“Neeru <neeram85@gmail.com>
2020-08-03 09:13:14 -0600

    Create Flight model

---
+++ spec/models/flights_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Flight, type: :model do
+  describe 'Validations' do
+    it {should validate_presence_of :number}
+    it {should validate_presence_of :date}
+    it {should validate_presence_of :time}
+    it {should validate_presence_of :arrival_city}
+    it {should validate_presence_of :departure_city}
+  end
+end
