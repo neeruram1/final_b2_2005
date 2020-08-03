9dc94b7cd65eea5529f40c83481d7edfae333428
“Neeru <neeram85@gmail.com>
2020-08-03 09:17:22 -0600

    Create Passenger table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200803151347) do
+ActiveRecord::Schema.define(version: 20200803151658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20200803151347) do
     t.string "time"
     t.string "departure_city"
     t.string "arrival_city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
